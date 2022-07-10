c825046c19d8ac2f46a6bc4f2b658c4938122571
maslinskimichal <maslinskimichal@gmail.com>
2022-07-10 11:12:41 +0200

    Initial commit

---
+++ src/test/java/konfiguracja/Main01.java
@@ -0,0 +1,27 @@
+package konfiguracja;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+
+public class Main01 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.google.com");
+
+        WebElement agreeButton = driver.findElement(By.id("L2AGLb"));
+        agreeButton.click();
+
+        WebElement element = driver.findElement(By.name("q"));
+        element.clear();
+        element.sendKeys("Allegro");
+        element.submit();
+        driver.quit();
+    }
+
+}
---
+++ src/test/java/konfiguracja/Main02.java
@@ -0,0 +1,17 @@
+package konfiguracja;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main02 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://coderslab.pl/pl");
+        driver.get("https://mystore-testlab.coderslab.pl/index.php");
+        driver.get("https://hotel-testlab.coderslab.pl/en/");
+        driver.quit();
+    }
+}
